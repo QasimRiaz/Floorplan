690a09e1e158df25f0b8cf812390adf477e61356
QasimRiaz <qasim.riaz@e2esp.com>
2021-11-17 12:53:38 +0500

    Update floorplan.php

--- floorplan.php
+++ floorplan.php
@@ -4,7 +4,7 @@
  * Plugin Name: Floor Plan
  * Plugin URI: https://github.com/QasimRiaz/Floorplan
  * Description: Floor Plan.
- * Version: 5.28
+ * Version: 5.29
  * Author: E2ESP
  * Author URI: http://expo-genie.com/
  * GitHub Plugin URI: https://github.com/QasimRiaz/Floorplan
