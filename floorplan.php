a9053c318f80b19d8c277cbcd13d3a9db13975a9
QasimRiaz <qasim.riaz@e2esp.com>
2023-01-12 17:14:15 +0500

    fixed release for new

--- floorplan.php
+++ floorplan.php
@@ -4,8 +4,8 @@
  * Plugin Name: Floor Plan
  * Plugin URI: https://github.com/QasimRiaz/Floorplan
  * Description: Floor Plan.
- * Version: 8.21
- * @version : 8.21
+ * Version: 8.22
+ * @version : 8.22
  * Author: E2ESP
  * Author URI: http://expo-genie.com/
  * GitHub Plugin URI: https://github.com/QasimRiaz/Floorplan
@@ -1699,24 +1699,24 @@ function floorplan_contentmanagerlogging($acction_name, $action_type, $pre_actio
 require 'plugin-update-checker/plugin-update-checker.php';
 use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
 
-include_once('updater.php');
+//include_once('updater.php');
 
 
 if (is_admin()) { // note the use of is_admin() to double check that this is happening in the admin
-       $config = array(
-           'slug' => plugin_basename(__FILE__), // this is the slug of your plugin
-           'proper_folder_name' => 'floorplan', // this is the name of the folder your plugin lives in
-           'api_url' => 'https://api.github.com/repos/QasimRiaz/Floorplan', // the GitHub API url of your GitHub repo
-           'raw_url' => 'https://raw.github.com/QasimRiaz/Floorplan/master', // the GitHub raw url of your GitHub repo
-           'github_url' => 'https://github.com/QasimRiaz/Floorplan', // the GitHub url of your GitHub repo
-           'zip_url' => 'https://github.com/QasimRiaz/Floorplan/zipball/master', // the zip url of the GitHub repo
-           'sslverify' => true, // whether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
-           'requires' => '3.0', // which version of WordPress does your plugin require?
-           'tested' => '3.3', // which version of WordPress is your plugin tested up to?
-           'readme' => 'README.md', // which file to use as the readme for the version number
-           'access_token' => '', // Access private repositories by authorizing under Appearance > GitHub Updates when this example plugin is installed
-       );
-       new WP_GitHub_floorplan_Updater($config);
+//        $config = array(
+//            'slug' => plugin_basename(__FILE__), // this is the slug of your plugin
+//            'proper_folder_name' => 'floorplan', // this is the name of the folder your plugin lives in
+//            'api_url' => 'https://api.github.com/repos/QasimRiaz/Floorplan', // the GitHub API url of your GitHub repo
+//            'raw_url' => 'https://raw.github.com/QasimRiaz/Floorplan/master', // the GitHub raw url of your GitHub repo
+//            'github_url' => 'https://github.com/QasimRiaz/Floorplan', // the GitHub url of your GitHub repo
+//            'zip_url' => 'https://github.com/QasimRiaz/Floorplan/zipball/master', // the zip url of the GitHub repo
+//            'sslverify' => true, // whether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
+//            'requires' => '3.0', // which version of WordPress does your plugin require?
+//            'tested' => '3.3', // which version of WordPress is your plugin tested up to?
+//            'readme' => 'README.md', // which file to use as the readme for the version number
+//            'access_token' => '', // Access private repositories by authorizing under Appearance > GitHub Updates when this example plugin is installed
+//        );
+//        new WP_GitHub_floorplan_Updater($config);
 //    Code by Zaeem
     $oldvalues = get_option('ContenteManager_Settings');
     $gitAuthKey = $oldvalues['ContentManager']['gitAuthKey'];
