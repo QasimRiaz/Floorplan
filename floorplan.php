d829805f64c4c0ef43d07406a6afcf2334086f38
Zaeem <32678310+Zaeem6100@users.noreply.github.com>
2023-02-02 12:36:41 +0500

    Update floorplan.php

--- floorplan.php
+++ floorplan.php
@@ -4,7 +4,7 @@
  * Plugin Name: Floor Plan
  * Plugin URI: https://github.com/QasimRiaz/Floorplan
  * Description: Floor Plan.
- * Version: 9.4
+ * Version: 9.5
  * @version : 9.4
  * Author: E2ESP
  * Author URI: http://expo-genie.com/
