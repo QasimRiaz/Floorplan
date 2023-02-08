d841f6e7e0dacfbf15d2b0022e16e84a82aa6005
Zaeem <32678310+Zaeem6100@users.noreply.github.com>
2023-02-08 16:25:10 +0500

    Update floorplan.php

--- floorplan.php
+++ floorplan.php
@@ -1740,19 +1740,17 @@ if (is_admin()) { // note the use of is_admin() to double check that this is hap
     // new WP_GitHub_floorplan_Updater($config);
 
 
-    $tokennumber ="expo-2023-"."FJr4Fa1i9RBzK7hbPRvDpRNfcrWUBi0EJ6c2";
-    $gitAuthKey = 'ghp_'.str_replace("expo-2023-",'',$tokennumber);
-
-    
-    $myUpdateChecker = PucFactory::buildUpdateChecker(
+      $gitKey = get_option("eg_gitauth_key");
+    if (!empty($gitkey)) {
+        $myUpdateChecker = PucFactory::buildUpdateChecker(
         'https://github.com/QasimRiaz/Floorplan',
         __FILE__,
         'FloorPlan'
     );
-
     $myUpdateChecker->setBranch('master');
-    $myUpdateChecker->setAuthentication($gitAuthKey);
+    $myUpdateChecker->setAuthentication($gitKey);
     $myUpdateChecker->getVcsApi()->enableReleaseAssets();
+    }
 
 
 
