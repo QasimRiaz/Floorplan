3cb4e342a5557e00497c63f155ed8b00acd2af1f
Shehroze <70812974+shehroze07@users.noreply.github.com>
2023-10-13 10:09:28 +0500

    Update floorplan.php

--- floorplan.php
+++ floorplan.php
@@ -4,8 +4,8 @@
  * Plugin Name: Floor Plan
  * Plugin URI: https://github.com/QasimRiaz/Floorplan
  * Description: Floor Plan.
- * Version: 11.5
- * @version : 11.5
+ * Version: 12.0
+ * @version : 12.0
  * Author: E2ESP
  * Author URI: http://expo-genie.com/
  * GitHub Plugin URI: https://github.com/QasimRiaz/Floorplan
