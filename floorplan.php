47294db522673f1cfde6f587b98ea2e780738991
Shehroze <70812974+shehroze07@users.noreply.github.com>
2023-12-11 14:40:04 +0500

    Update floorplan.php

--- floorplan.php
+++ floorplan.php
@@ -2079,7 +2079,7 @@ if (is_admin()) { // note the use of is_admin() to double check that this is hap
             __FILE__,
             'FloorPlan'
         );
-        $myUpdateChecker->setBranch('Shehroze-Dev');
+        $myUpdateChecker->setBranch('master');
         $myUpdateChecker->setAuthentication($gitKey);
         $myUpdateChecker->getVcsApi()->enableReleaseAssets();
     }
