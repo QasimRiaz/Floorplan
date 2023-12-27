786c6bc88337d5f7d7bb3c1bce3cad926f53b12b
Shehroze <shehrozearif70@gmail.com>
2023-12-27 10:31:02 +0500

    fixed print preview with exhibitors

--- floorplan.php
+++ floorplan.php
@@ -4,8 +4,8 @@
  * Plugin Name: Floor Plan
  * Plugin URI: https://github.com/QasimRiaz/Floorplan
  * Description: Floor Plan.
- * Version: 12.01
- * @version : 12.01
+ * Version: 12.02
+ * @version : 12.02
  * Author: E2ESP
  * Author URI: http://expo-genie.com/
  * GitHub Plugin URI: https://github.com/QasimRiaz/Floorplan
