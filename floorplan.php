8e566f8bcda18dcc6196cbeb7a688bb288084765
Shehroze <shehrozearif70@gmail.com>
2024-03-20 14:03:41 +0500

    hide exhibitor details issue fixed

--- floorplan.php
+++ floorplan.php
@@ -4,8 +4,8 @@
  * Plugin Name: Floor Plan
  * Plugin URI: https://github.com/QasimRiaz/Floorplan
  * Description: Floor Plan.
- * Version: 13.05
- * @version : 13.05
+ * Version: 13.06
+ * @version : 13.06
  * Author: E2ESP
  * Author URI: http://expo-genie.com/
  * GitHub Plugin URI: https://github.com/QasimRiaz/Floorplan
