c201c32cab87a81933664fcfd48a3dc6df0d66db
Shehroze <shehrozearif70@gmail.com>
2024-03-25 17:20:32 +0500

    hide exhibitor detail on public view issue fixed

--- floorplan.php
+++ floorplan.php
@@ -4,8 +4,8 @@
  * Plugin Name: Floor Plan
  * Plugin URI: https://github.com/QasimRiaz/Floorplan
  * Description: Floor Plan.
- * Version: 13.09
- * @version : 13.09
+ * Version: 13.10
+ * @version : 13.10
  * Author: E2ESP
  * Author URI: http://expo-genie.com/
  * GitHub Plugin URI: https://github.com/QasimRiaz/Floorplan
