3352783eeba83b67ffbb8a66078d3d9c1dc934d5
FAZubair <fazubair@gmail.com>
2024-06-20 15:50:39 +0500

    New Release Features
    New Release Features

--- floorplan.php
+++ floorplan.php
@@ -4,8 +4,8 @@
  * Plugin Name: Floor Plan
  * Plugin URI: https://github.com/QasimRiaz/Floorplan
  * Description: Floor Plan.
- * Version: 15.02
- * @version : 15.02
+ * Version: 15.03
+ * @version : 15.03
  * Author: E2ESP
  * Author URI: http://expo-genie.com/
  * GitHub Plugin URI: https://github.com/QasimRiaz/Floorplan
