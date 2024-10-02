4f482ebf4b1fabe735ae2fbf7e6132529ae6ad71
FAZubair <fazubair@gmail.com>
2024-10-02 11:37:50 +0500

    Floorplan version updated to 17.08
    Floorplan version updated to 17.08

--- floorplan.php
+++ floorplan.php
@@ -4,8 +4,8 @@
  * Plugin Name: Floor Plan
  * Plugin URI: https://github.com/QasimRiaz/Floorplan
  * Description: Floor Plan.
- * Version: 17.07
- * @version : 17.07
+ * Version: 17.08
+ * @version : 17.08
  * Author: E2ESP
  * Author URI: http://expo-genie.com/
  * GitHub Plugin URI: https://github.com/QasimRiaz/Floorplan
